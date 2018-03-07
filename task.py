a4a0f0a8b8052a78c006e18f97e9f96f2730ccb1
Przemysław Szopa <prz.szo@gmail.com>
2018-03-07 18:40:49 +0100

    Task

--- task.py
+++ task.py
@@ -1,26 +1,25 @@
-#Write a library that contains a class that can represent any 2𝑥2 real matrice. 
-#Include two functions to calculate the sum and product of two matrices. 
-#Next, write a program that imports this library module and use it to perform calculations.
-#Examples:
-#
-# matrix_1 = Matrix(4,5,6,7)
-# matrix_2 = Matrix(2,2,2,1)
-#
-# matrix_3 = matrix_2.add(matrix_1)
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#(If you want you can expand implementation to NxN matrix.)
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#
-#When you are done upload this code to your github repository. 
-#The whole repository MUST be a fork from https://github.com/mwmajew/kol1_gr1.py
-#
-#Delete these comments before commit!
-#Good luck.
+# coding=utf-8
+
+class Matrix(object):
+	def __init__(self, listOfData):
+		super(Matrix, self).__init__()
+		self.listOfData = [[listOfData[0], listOfData[1]],
+											 [listOfData[2], listOfData[3]]]
+
+	def set(self, row, col, value):
+		self.listOfData[row][col] = value
+
+	def add(self, toAdd):
+		return Matrix([self.listOfData[0][0]+toAdd.listOfData[0][0],
+			self.listOfData[0][1]+toAdd.listOfData[0][1],
+			self.listOfData[1][0]+toAdd.listOfData[1][0],
+			self.listOfData[1][1]+toAdd.listOfData[1][1]])		
+
+	def multiply(self, toMultiply):
+		result = Matrix([0,0,0,0])
+		for i in range(0,3):
+			for j in range(0,3):
+				for k in range(0,3):
+					result.set(i, j, (self.listOfData[i][k]*toMultiply.listOfData[k][j]))
+		return result
+
