119b8c9e129075f6f8a1339b6f70199b2fe7d00b
przszo <prz.szo@gmail.com>
2018-03-07 18:38:27 +0100

    The tast I have made

---
+++ main.py
@@ -0,0 +1,11 @@
+from task import Matrix
+
+matrixA = Matrix([1,2,3,4])
+matrixB = Matrix([1,2,3,4])
+
+print(matrixA.listOfData)
+
+print(matrixA.add(matrixB).listOfData)
+
+print(matrixA.multiply(matrixB).listOfData)
+
