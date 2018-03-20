946f1db4df7ce67b74614f3895e10a6cd8dfb781
przszo <prz.szo@gmail.com>
2018-03-20 22:44:00 +0100

    Matrix class has been improved

--- main.py
+++ main.py
@@ -1,11 +1,31 @@
 from task import Matrix
 
-matrixA = Matrix([1,2,3,4])
-matrixB = Matrix([1,2,3,4])
+matrixA = Matrix(2,2,2,2)
+matrixB = Matrix(1,0,0,1)
 
-print(matrixA.listOfData)
+print("Matrix A")
+print(matrixA)
 
-print(matrixA.add(matrixB).listOfData)
+print("Matrix B")
+print(matrixB)
 
-print(matrixA.multiply(matrixB).listOfData)
+print("A + B")
+print(matrixA + matrixB)
 
+print("A + 1")
+print(matrixA + 1)
+
+print("1 + A")
+print(1 + matrixA)
+
+print("A * B")
+print(matrixA*matrixB)
+
+print("A * 2")
+print(matrixA*2)
+
+print("A * 2")
+print(2*matrixA)
+
+print("Construct")
+print(Matrix.construct(1,2,3,4))
--- task.py
+++ task.py
@@ -1,25 +1,80 @@
-# coding=utf-8
+from math import sqrt
 
-class Matrix(object):
-	def __init__(self, listOfData):
-		super(Matrix, self).__init__()
-		self.listOfData = [[listOfData[0], listOfData[1]],
-											 [listOfData[2], listOfData[3]]]
 
-	def set(self, row, col, value):
-		self.listOfData[row][col] = value
+class Matrix:
+	def __init__(self, *numbers):
+		matrix_size = sqrt(len(numbers))
+		if matrix_size.is_integer():
+			self.list_of_data = list(numbers)
+			self.matrix_size = int(matrix_size)
+		else:
+			raise ValueError('Cannot create Matrix from those arguments!')
 
-	def add(self, toAdd):
-		return Matrix([self.listOfData[0][0]+toAdd.listOfData[0][0],
-			self.listOfData[0][1]+toAdd.listOfData[0][1],
-			self.listOfData[1][0]+toAdd.listOfData[1][0],
-			self.listOfData[1][1]+toAdd.listOfData[1][1]])		
+	@staticmethod
+	def construct(*numbers):
+		matrix_size = sqrt(len(numbers))
+		if matrix_size.is_integer():
+			return Matrix(*numbers)
+		else:
+			raise ValueError('Cannot create Matrix from those arguments!')
 
-	def multiply(self, toMultiply):
-		result = Matrix([0,0,0,0])
-		for i in range(0,3):
-			for j in range(0,3):
-				for k in range(0,3):
-					result.set(i, j, (self.listOfData[i][k]*toMultiply.listOfData[k][j]))
-		return result
+	def add(self, to_add):
+		if (self == to_add):
+			return Matrix(*[self[x]+to_add[x] for x in range(self.matrix_size**2)])
+		else:
+			raise ValueError('Cannot add diffrent size matrices!')
 
+	def __add__(self, to_add):
+		if type(to_add) == Matrix:
+			return self.add(to_add)
+		elif type(to_add) == str:
+			raise ValueError('Cannot adding string to the Matrix!')
+		else:
+			matrix_of_value = Matrix(*(to_add for x in range(self.matrix_size**2)))
+			return self.add(matrix_of_value)
+
+	def __radd__(self, to_add):
+		return self + to_add
+
+	def multiply_by(self, to_multiply):
+		if (self == to_multiply):
+			size = self.matrix_size
+			result_matrix = Matrix(*(0 for x in range(size**2)))
+			for row in range(size):
+				for col in range(size):
+					value_of_result_matrix = 0
+					for k in range(size):
+						value_of_result_matrix += self[row*size + k] * to_multiply[k*size + col]
+					result_matrix[row*size + col] = value_of_result_matrix
+			return result_matrix
+		else:
+			raise ValueError("Cannot multiplying diffrent size matrices")
+
+	def __mul__(self, to_multiply):
+		if type(to_multiply) == Matrix:
+			return self.multiply_by(to_multiply)
+		elif type(to_multiply) == str:
+			raise ValueError('Cannot multiplying Matrix with the Matrix!')
+		else:
+			matrix_of_value = Matrix(*(to_multiply for x in range(self.matrix_size**2)))
+			return self.multiply_by(matrix_of_value)
+
+	def __rmul__(self, to_multiply):
+		return self * to_multiply
+
+	def __str__(self):
+		size = self.matrix_size
+		return "\n".join([" ".join(
+			["{}".format(self[row*size + col]) for col in range(size)]) for row in range(size)])
+
+	def __getitem__(self, index):
+		return self.list_of_data[index]
+
+	def __setitem__(self, index, value):
+		self.list_of_data[index] = value
+
+	def __eq__(self, another_matrix):
+		return self.matrix_size == another_matrix.matrix_size
+
+if __name__ == "__main__":
+	import main
